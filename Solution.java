0726be55c0b4ece665fe9dfd17bd29bb0e7fc7ac
kimgoyang <ehxhfl643@naver.com>
2025-07-26 23:25:50 +0900

    update sample solution

---
+++ Solution.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		// System.setIn(new FileInputStream("res/input.txt"));
+
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		}
+
+        sc.close();
+	}
+}

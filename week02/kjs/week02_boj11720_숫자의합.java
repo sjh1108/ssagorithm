1e372e6c866c37359d8239048140f16301c0a1d7
Gabriel-Ki <gabriel8552@gmail.com>
2025-08-17 20:30:54 +0900

    kjs v1.0 (#17)

---
+++ week02/kjs/week02_boj11720_숫자의합.java
@@ -0,0 +1,26 @@
+package ssagorithm.week02;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+
+public class week02_boj11720_숫자의합 {
+
+	   public static void main(String[] args) throws IOException {
+
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        int N = Integer.parseInt(br.readLine());
+	        String numbers = br.readLine();
+
+	        int sum = 0;
+
+	        for (int i = 0; i < N; i++) {
+	            sum += numbers.charAt(i) - '0';
+	        }
+
+	        System.out.println(sum);
+	    }
+
+}

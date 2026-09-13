ca1aa43eec6ac6586425c8f8f621fc1335e46609
sjh1108 <ch6854@naver.com>
2026-09-13 12:34:42 +0900

    [DOCS] PR 템플릿 수정

--- _20260914/thdwngjs/set01A/Main.java
+++ _20260914/thdwngjs/set01A/Main.java
@@ -10,6 +10,7 @@ public class Main {
     int M = Integer.parseInt(st.nextToken());
 
     Queue<int[]> q = new ArrayDeque<>();
+
     char[][] map = new char[N][M];
     for(int i = 0; i < N; i++){
       map[i] = br.readLine().toCharArray();
